f76fb743696edc072c8df06dcc0759dae1c3cee0
Dikshit <gautamdixit120@gmail.com>
2023-05-27 10:38:47 +0545

    Database file added

---
+++ database.sql
@@ -0,0 +1,16 @@
+-- Create database
+CREATE DATABASE futsal;
+
+-- -- Use database
+-- USE futsal;
+
+-- Create table
+CREATE TABLE registration (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  first_name VARCHAR(50) NOT NULL,
+  last_name VARCHAR(50) NOT NULL,
+  team_name VARCHAR(50) UNIQUE NOT NULL,
+  email VARCHAR(100) PRIMARY NOT NULL,
+  contact_no VARCHAR(10) NOT NULL,
+  password VARCHAR(100) NOT NULL
+);
